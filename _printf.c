164f795fd73b2cca3f8bef8e60bbb3773dacb2f3
laila <laila.mf2005@gmail.com>
2024-07-27 14:49:04 -0700

    task1

--- _printf.c
+++ _printf.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * _printf - Produces output according to a format
- * @format: A character string composed of zero or more directives
- *
- * Return: The number of characters printed (excluding null byte)
- */
+* _printf - Produces output according to a format
+* @format: A character string composed of zero or more directives
+*
+* Return: The number of characters printed (excluding null byte)
+*/
 int _printf(const char *format, ...)
 {
 va_list args;
@@ -44,5 +44,42 @@ else
 write(1, p, 1), count++;
 }
 va_end(args);
+return (count);
+}
+
+/**
+* print_number - Prints an integer to standard output
+* @n: The integer to print
+*
+* Return: The number of characters printed
+*/
+int print_number(int n)
+{
+int count = 0;
+char buf[12];
+int i = 0;
+unsigned int num;
+
+if (n < 0)
+{
+write(1, "-", 1);
+count++;
+num = -n;
+}
+else
+{
+num = n;
+}
+do {
+buf[i++] = (num % 10) + '0';
+num /= 10;
+} while (num > 0);
+
+while (i--)
+{
+write(1, &buf[i], 1);
+count++;
+}
+
 return (count);
 }
